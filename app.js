bc2ab35aedede7ed02b677241d1d67bc11d321f9
Carlos Valderrama <cvalderramav@gmail.com>
2016-08-11 05:18:05 -0500

    Update models

--- app.js
+++ app.js
@@ -1,21 +1,36 @@
-var express = require("express"),  
-    app = express(),
-    bodyParser  = require("body-parser"),
-    methodOverride = require("method-override");
-    mongoose = require('mongoose');
+var express  = require("express"),  
+    app      = express(),
+    http     = require("http"),
+    server   = http.createServer(app),
+    mongoose = require('mongoose'),
+    bodyParser = require('body-parser'),
+    router = express.Router(),
+    methodOverride = require('method-override');
 
-app.use(bodyParser.urlencoded({ extended: false }));  
-app.use(bodyParser.json());  
-app.use(methodOverride());
-
-var router = express.Router();
+app.use(router);
+app.use(bodyParser.json());   
+app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(methodOverride(function(req, res){
+  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+    // look in urlencoded POST bodies and delete it
+    var method = req.body._method
+    delete req.body._method
+    return method
+  }
+}));
 
 router.get('/', function(req, res) {  
    res.send("Hello World!");
 });
 
-app.use(router);
-
-app.listen(3000, function() {  
-  console.log("Node server running on http://localhost:3000");
+mongoose.connect('mongodb://localhost/tvshows', function(err, res) {  
+  if(err) {
+    console.log('ERROR: connecting to Database. ' + err);
+  }
+  else{
+  	console.log('Connected to Database');
+  }
+  app.listen(3000, function() {
+    console.log("Node server running on http://localhost:3000");
+  });
 });
